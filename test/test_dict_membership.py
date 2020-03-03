932305fa07f27cbf5a9b7db21fe1124c519e9232
Zachary Hayes <zachary.j.hayes@gmail.com>
2020-03-03 16:26:25 -0600

    Fixed tests, still fail

--- test/test_dict_membership.py
+++ test/test_dict_membership.py
@@ -4,14 +4,14 @@ from more_fun_with_collections import dict_membership
 
 class MyTestCase(unittest.TestCase):
     def test_in_dict_true(self):
-        test_set = {1, 2, 3, 4, 5}
+        test_set = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5}
         item_to_search = 5
-        self.assertTrue(dict_membership.in_set(test_set, item_to_search))
+        self.assertTrue(dict_membership.in_dict(test_set, item_to_search))
 
     def test_in_dict_false(self):
-        test_set = {1, 2, 3, 4}
+        test_set = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5}
         item_to_search = 5
-        self.assertFalse(dict_membership.in_set(test_set, item_to_search))
+        self.assertFalse(dict_membership.in_dict(test_set, item_to_search))
 
 
 if __name__ == '__main__':
