4714e12b35f90e6eceb8dd731b46a3c9278bdb8a
Zachary Hayes <zachary.j.hayes@gmail.com>
2020-03-03 16:11:07 -0600

    test_in_set_true FAILS

---
+++ more_fun_with_collections/set_membership.py
@@ -0,0 +1,5 @@
+# Zachary Hayes
+
+
+def in_set(set, item_to_check):
+    pass
--- test/test_set_membership.py
+++ test/test_set_membership.py
@@ -1,9 +1,12 @@
 import unittest
+from more_fun_with_collections import set_membership
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_in_set_true(self):
+        test_set = {1, 2, 3, 4, 5}
+        item_to_check = 5
+        self.assertTrue(set_membership.in_set(test_set, item_to_check))
 
 
 if __name__ == '__main__':
