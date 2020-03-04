3303f1839ecf44797e90cc818c4da92c50e25eaf
Zachary Hayes <zachary.j.hayes@gmail.com>
2020-03-04 14:05:41 -0600

    A passes

--- more_fun_with_collections/assign_average.py
+++ more_fun_with_collections/assign_average.py
@@ -1,4 +1,9 @@
 # Zachary Hayes
 
-def switch_average():
-    pass
+
+def switch_average(grade):
+    switch = {
+        'A': 90
+    }
+    return switch.get(grade)
+
--- test/test_assign_average.py
+++ test/test_assign_average.py
@@ -4,28 +4,28 @@ from more_fun_with_collections import assign_average
 
 class MyTestCase(unittest.TestCase):
     def test_a(self):
-        actual = assign_average.switch_average(90)
-        self.assertEqual(actual, 'A')
+        actual = assign_average.switch_average('A')
+        self.assertEqual(actual, 90)
 
     def test_b(self):
-        actual = assign_average.switch_average(80)
-        self.assertEqual(actual, 'B')
+        actual = assign_average.switch_average('B')
+        self.assertEqual(actual, 80)
 
     def test_c(self):
-        actual = assign_average.switch_average(70)
-        self.assertEqual(actual, 'C')
+        actual = assign_average.switch_average('C')
+        self.assertEqual(actual, 70)
 
     def test_d(self):
-        actual = assign_average.switch_average(60)
-        self.assertEqual(actual, 'D')
+        actual = assign_average.switch_average('D')
+        self.assertEqual(actual, 60)
 
     def test_d(self):
-        actual = assign_average.switch_average(50)
-        self.assertEqual(actual, 'F')
+        actual = assign_average.switch_average('F')
+        self.assertEqual(actual, 50)
 
     def test_invalid(self):
-        with self.assertRaises(ValueError):
-            assign_average.switch_average('F')
+        actual = assign_average.switch_average('X')
+        self.assertEqual(actual, 'Invalid grade')
 
 
 if __name__ == '__main__':
